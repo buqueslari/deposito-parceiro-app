d4eb3b36a2e8928e5093fd5ef157739aa27ad102
buqueslari <vitosanti22@gmail.com>
2026-05-22 17:20:31 -0300

    Set conversion label AW-18181769409/KFMxCIjZy7EcEMGR391D

--- src/views/OrderSuccessView.vue
+++ src/views/OrderSuccessView.vue
@@ -366,7 +366,7 @@ async function startPolling() {
           track('payment_confirmed', { order_id: rawId, total: fresh?.total })
           if (typeof window.gtag === 'function') {
             window.gtag('event', 'conversion', {
-              send_to: 'AW-18181769409',
+              send_to: 'AW-18181769409/KFMxCIjZy7EcEMGR391D',
               value: fresh?.total ?? 0,
               currency: 'BRL',
               transaction_id: rawId,
